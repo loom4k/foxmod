129859b217c0663fef2e7bf66936d3c0cacc5970
Loom4k <loom4k.twitch@gmail.com>
2021-05-05 20:34:18 -0400

    Update on Slash-Commands

---
+++ src/commands/kick.ts
@@ -0,0 +1,39 @@
+import { Guild, MessageEmbed } from "discord.js";
+import { ICommand } from "my-module";
+import { CONFIG } from "../config";
+
+const InfoCommand: ICommand = {
+	name: "kick",
+	description: "Test kickkk",
+	options: [],
+	async execute({ client, interaction }) {
+		const member = (client.guilds.cache.get(
+			interaction.guild_id,
+		) as Guild).member(interaction.member.user.id.toString());
+		const embed = new MessageEmbed()
+			.setColor(member ? member.displayHexColor : "GREEN")
+			.setAuthor(
+				"Thank you for choosing FoxMod",
+				client.user?.displayAvatarURL(),
+				CONFIG.SLASH_INVITE,
+			)
+			.setFooter("Made with <3 by Loom4k")
+			.addField("1 - Add Commands", `[Click!](${CONFIG.SLASH_INVITE})`)
+			.addField("2 - Invite Bot", `[Click!](${CONFIG.DEFAULT_INVITE})`)
+			.addField(
+				"3 - Support Server",
+				`[Click!](${CONFIG.SUPPORT_SERVER})`,
+			)
+			.setDescription(
+				"FoxMod is the most powerful moderation bot using Slash Commands you can find on Discord. It's built with the new Discord API integration and more applications are coming everyday. Enjoy and share with your friends!",
+			)
+			.setImage("https://bariscodes.me/slash-example.gif");
+		return client.send(
+			interaction,
+			"✨ The only Slash-Commands moderation bot you need! ✨",
+			embed,
+		);
+	},
+};
+
+export default InfoCommand;
